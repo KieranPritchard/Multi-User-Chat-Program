4a8903c46202bc3f5413a13c7f86498a923354b4
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-02-25 21:00:07 +0000

    Created server socket object

--- server.py
+++ server.py
@@ -3,3 +3,11 @@ import socket
 # Server configuration
 HOST = '127.0.0.1'
 PORT = 12345
+server_config = ((HOST, PORT))
+
+#Function to start server
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(server_config)
+    server_socket.listen()
+    print(f"Server is listioning on {HOST}:{PORT}")
