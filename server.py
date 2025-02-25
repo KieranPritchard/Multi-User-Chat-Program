7b1ecc998e6761e6b0e85bea16129668fcd7ea26
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-02-25 21:07:04 +0000

    Added an array to keep track of users

--- server.py
+++ server.py
@@ -5,9 +5,16 @@ HOST = '127.0.0.1'
 PORT = 12345
 server_config = ((HOST, PORT))
 
+# Keeps a list of all the connected clients
+clients = []
+
 #Function to start server
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(server_config)
     server_socket.listen()
     print(f"Server is listioning on {HOST}:{PORT}")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        clients.append(client_socket)
