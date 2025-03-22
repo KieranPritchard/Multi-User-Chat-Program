caa79f9d97966ce0d1a96fd88906a9fe9727b507
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-03-22 14:03:05 +0000

    Added error handing.

--- client.py
+++ client.py
@@ -8,20 +8,24 @@ server_address_information = (HOST, PORT)
 # This creates the client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.bind(server_address_information)
-client_socket.connect()
 
-while True:
-    # This takes in user input then formats and checks it for the quit message
-    message_input = input("Please type your message: ").lower()
-    if message_input != "quit":
-        client_socket.send(message_input.encode())
-        message_receive = client_socket.recv(1024)
-        if not message_receive:
-            print("The server closed the connection.")
-            break
+try:
+    client_socket.connect()
 
-        print(message_receive.decode())
-    else:
-        # This closes the connection if the 
-        print("Closing connection.")
-        client_socket.close()
+    while True:
+        # This takes in user input then formats and checks it for the quit message
+        message_input = input("Please type your message: ").lower()
+        if message_input != "quit":
+            client_socket.send(message_input.encode())
+            message_receive = client_socket.recv(1024)
+            if not message_receive:
+                print("The server closed the connection.")
+                break
+
+            print(message_receive.decode())
+        else:
+            # This closes the connection if the 
+            print("Closing connection.")
+            client_socket.close()
+except Exception as e:
+    print(f"Encountered Error {e}")
