9cf91fda3416bd50a4df6438137e7deac2980075
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-03-22 13:57:10 +0000

    Added data sending to server.

--- client.py
+++ client.py
@@ -7,3 +7,17 @@ server_address_information = (HOST, PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.bind(server_address_information)
 client_socket.connect()
+
+while True:
+    message_input = input("Please type your message: ").lower()
+    if message_input != "quit":
+        client_socket.send(message_input.encode())
+        message_receive = client_socket.recv(1024)
+        if not message_receive:
+            print("The server closed the connection.")
+            break
+
+        print(message_receive.decode())
+    else:
+        print("Closing connection.")
+        client_socket.close()
