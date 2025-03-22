773e2fdecbb6a80f07b3cbe858cd2596dbba57ef
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-03-22 13:40:50 +0000

    Created client socket.

--- client.py
+++ client.py
@@ -3,3 +3,7 @@ import socket
 HOST = "127.0.0.1"
 PORT = 12345
 server_address_information = (HOST, PORT)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.bind(server_address_information)
+client_socket.connect()
